64ddb8e97d9d7cba9964e7ee282f10ed64c80c6b
incremental capital <incrementalcapital@gmail.com>
2024-08-06 15:26:01 +0900

    add test lambda function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { groqQueryFunction } from './groq-query-function/resource';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 defineBackend({
+  groqQueryFunction,
   auth,
   data,
 });
